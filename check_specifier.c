e43b420980b816673620f99d809ef86fb1a789e1
iamqaasim <iamqaasim@gmail.com>
2022-07-11 10:56:23 -0700

    My first commit

--- check_specifier.c
+++ check_specifier.c
@@ -5,25 +5,8 @@
  *
  * Return: pointer to a function
  */
- 
-int (*check_specifier(char *format))(va_list)
+
+int (*check_specifier(char*))(va_list)
 {
-	int i;
-
-	func_t my_array[4] = 
-	{
-		{'c', print_char},
-		{'s', ptint_str},
-		{'%', print_cent},
-		{NULL, NULL}
-	};
-
-	for (i = 0; my_array[i}.t != NULL; i++)
-	{
-		if (my_array[i].t == *format)
-		{
-			return (my_array[i].f);
-		}
-	}
-	return (NULL);
-}
+        //expressions;
+}  
