56be29c273d21565d056597d4f7c7ce09f4ff245
iamqaasim <iamqaasim@gmail.com>
2022-07-12 12:15:04 -0700

    My first commit

--- _printf.c
+++ _printf.c
@@ -1,44 +1,51 @@
-#include <stdarg.h>
-#include <stdioi.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * _printf - prints a string in a formated way
- * @format: string to print (char *)
- * @...: variadic parameters
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
  *
- * Return:number of characters printed 
+ * Return: number of chars printed.
  */
-
-int _printf(const char* format, ...)
+int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int value = 0;
-	va_list args;
-	va_start(args, format); 
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	/*prevent passing a null pointer*/	
-	if (format == NULL)
-		return(-1);
-
-	/*print each character of string*/
-	while (format[i])
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
-		if(format[i] != '%')
-		{
-			value = write(1,&format[i],1);
-			count = count + value;
-			i++;
-			continue;
-		}
-
 		if (format[i] == '%')
 		{
-			//expressions;
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
 		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-	
-	return (count);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
